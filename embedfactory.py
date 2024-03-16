b095b01fc31ea7888a34104eb4941b1dd6d4ba64
mh4g20 <mh4g20@soton.ac.uk>
2024-03-16 22:52:31 +0000

    Update to Embed Factory messages

--- embedfactory.py
+++ embedfactory.py
@@ -6,43 +6,16 @@ from game import Card
 class EmbedFactory:
 
     def error(self, session_id: str, error_msg: str) -> Embed:
-        raise NotImplemented()
-
-    def waitForStart(self, session_id: str, player_count: int) -> Embed:
-        raise NotImplemented()
-
-    def waitForTurn(self, session_id: str, action: str) -> Embed:
-        raise NotImplemented()
-
-    def turn(self, session_id: str, cards: list[Card]) -> Embed:
-        raise NotImplemented()
-
-    def card_info(self, card: Card) -> Embed:
-        raise NotImplemented()
-
-    def game_won(self, session_id: str) -> Embed:
-        raise NotImplemented()
-
-    def game_lost(self, session_id: str) -> Embed:
-        raise NotImplemented()
-
-    def game_over(self, session_id: str) -> Embed:
-        raise NotImplemented()
-
-    def rules(self) -> Embed:
-        raise NotImplemented()
-
-    def create(self, session_id: str) -> Embed:
         return (
             EmbedBuilder()
-            .colour("white")
-            .title("Welcome to Stay Away!")
-            .description("You have successfully created a lobby")
-            .field("Session ID", session_id)
+            .colour("red")
+            .title("Error Occured!")
+            .description(error_msg)
+            .footer("Session ID: " + session_id)
             .embed()
         )
 
-    def wait(self, player_count: int, session_id: str) -> Embed:
+    def waitForStart(self, session_id: str, player_count: int) -> Embed:
         return (
             EmbedBuilder()
             .colour("white")
@@ -51,7 +24,69 @@ class EmbedFactory:
                 "You need a minimum of 4 players to start a game and can have a maximum of 12"
             )
             .field("Players", player_count)
-            .footer("Session ID: " + session_id)
+            .footer(f"Session ID: {session_id}")
+            .embed()
+        )
+
+    def waitForTurn(self, session_id: str, action: str) -> Embed:
+        return (
+            EmbedBuilder()
+            .colour("white")
+            .title("Waiting for your turn...")
+            .description(action)
+            .footer(f"Session ID: {session_id}")
+            .embed()
+        )
+
+    def turn(self, session_id: str, cards: list[Card]) -> Embed:
+        return (
+            EmbedBuilder()
+            .colour("white")
+            .title("It is your turn")
+            .description("")
+            .footer(f"Session ID: {session_id}")
+            .embed()
+        )
+
+    def card_info(self, card: Card) -> Embed:
+        return (
+            EmbedBuilder()
+            .colour("white")
+            .title(card.name)
+            .description(card.description)
+            .field("Card Type: ", card.kind)
+            .embed()
+        )
+
+    def game_won_humans(self, session_id: str) -> Embed:
+        return (
+            EmbedBuilder()
+            .colour("white")
+            .title("The Thing has lost the game!")
+            .description(
+                "The Thing has been eliminated and all remaining players are not infected."
+            )
+            .footer(f"Session ID: {session_id}")
+        )
+
+    def game_won_thing(self, session_id: str) -> Embed:
+        return (
+            EmbedBuilder()
+            .colour("white")
+            .title("The Thing has won the game!")
+            .description(
+                "All remaining players have are now infected, the Thing and the infected have won."
+            )
+            .footer(f"Session ID: {session_id}")
+            .embed()
+        )
+
+    def game_over(self, session_id: str) -> Embed:
+        return (
+            EmbedBuilder()
+            .title("Game Over!")
+            .description("The game has ended")
+            .footer(f"Session ID: {session_id}")
             .embed()
         )
 
@@ -65,36 +100,3 @@ class EmbedFactory:
             )
             .embed()
         )
-
-    def start(self, session_id: str) -> Embed:
-        return (
-            EmbedBuilder()
-            .colour("white")
-            .title("Game started!")
-            .description("You have all been dealt 4 cards")
-            .footer("Session ID: " + session_id)
-            .embed()
-        )
-
-    def error(self, message: str, session_id: str) -> Embed:
-        return (
-            EmbedBuilder()
-            .colour("red")
-            .title("Error Occured!")
-            .description(message)
-            .footer("Session ID: " + session_id)
-            .embed()
-        )
-
-    def draw(self, card_info: str, session_id: str) -> Embed:
-        return (
-            EmbedBuilder()
-            .colour("white")
-            .title("You have drawn the following card")
-            .description(card_info)
-            .footer("Session ID: " + session_id)
-            .embed()
-        )
-
-    def play():
-        raise NotImplemented()
