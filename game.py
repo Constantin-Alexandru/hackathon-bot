1e25795b2aab1e81582f29322642f7b96763a617
Kristiyan Yochev <kris.yochev@gmail.com>
2024-03-16 17:11:39 +0000

    Set up tests

--- game.py
+++ game.py
@@ -1,2 +1,36 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from __future__ import annotations
+
+
+@dataclass
+class Player:
+    pid: str
+
+
 class Game:
-    pass
+    _players: list[Player]
+
+    def __init__(self, player_ids: list[str]) -> None:
+        self._players = list(map(lambda pid: Player(pid), player_ids))
+
+    @property
+    def player_count(self) -> int:
+        return len(self._players)
+
+
+class CardType(Enum):
+    ROLE = auto()
+    PANIC = auto()
+    ACTION = auto()
+    DEFENSE = auto()
+
+
+@dataclass
+class Card:
+    name: str
+    description: str
+    kind: CardType
+
+    def play(self, game: Game) -> None:
+        raise NotImplementedError()
