699aae5570b9a8ef5fb65c7fbc1992d419dca89b
Kristiyan Yochev <kris.yochev@gmail.com>
2024-03-16 18:03:16 +0000

    Added deck tests

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum, auto
-from __future__ import annotations
 
 
 @dataclass
@@ -26,11 +26,34 @@ class CardKind(Enum):
     DEFENSE = auto()
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 class Card:
     name: str
     description: str
     kind: CardKind
 
     def play(self, game: Game) -> None:
+        raise NotImplementedError()
+
+
+class Deck:
+    _cards: list[Card]
+
+    def __init__(self, cards: list[Card]) -> None:
+        self._cards = cards
+
+    @property
+    def size(self) -> int:
+        return len(self._cards)
+
+    def draw(self) -> Card:
+        """Draw a card from the deck"""
+        raise NotImplementedError()
+
+    def add_card(self, card: Card) -> None:
+        """Add a card to the deck"""
+        raise NotImplementedError()
+
+    def shuffle(self) -> None:
+        """Shuffle the deck"""
         raise NotImplementedError()
--- game_test.py
+++ game_test.py
@@ -1,11 +1,105 @@
 import unittest
-from game import Game
+from game import *
 
 
 class GameTests(unittest.TestCase):
     def test_game_creation(self):
         game = Game(["1", "2", "3", "4"])
         self.assertEqual(game.player_count, 4)
+
+
+class DeckTests(unittest.TestCase):
+    def test_new_deck(self):
+        deck = Deck(
+            [
+                Card("The Thing", "You are The Thing", CardKind.ROLE),
+                Card(
+                    "Infected",
+                    "If you got this card from another player, you are infected!",
+                    CardKind.ROLE,
+                ),
+                Card("Flamethrower", "Kill an adjacent player", CardKind.ACTION),
+            ]
+        )
+
+        self.assertEqual(deck.size, 3)
+
+    def test_deck_shuffles(self):
+        deck = Deck(
+            [
+                Card("The Thing", "You are The Thing", CardKind.ROLE),
+                Card(
+                    "Infected",
+                    "If you got this card from another player, you are infected!",
+                    CardKind.ROLE,
+                ),
+                Card("Flamethrower", "Kill an adjacent player", CardKind.ACTION),
+            ]
+        )
+
+        size_before = deck.size
+        set_before = set(deck._cards)
+
+        deck.shuffle()
+
+        size_after = deck.size
+        set_after = set(deck._cards)
+        self.assertEqual(size_before, size_after, "The size of the deck changed")
+        self.assertEqual(
+            set_before, set_after, "The deck changed which cards are in it"
+        )
+
+    def test_deck_add_card(self):
+        deck = Deck(
+            [
+                Card("The Thing", "You are The Thing", CardKind.ROLE),
+                Card(
+                    "Infected",
+                    "If you got this card from another player, you are infected!",
+                    CardKind.ROLE,
+                ),
+                Card("Flamethrower", "Kill an adjacent player", CardKind.ACTION),
+            ]
+        )
+
+        size_before = deck.size
+        card_to_add = Card(
+            "Analysis", "See the hand of an adjacent player", CardKind.ACTION
+        )
+
+        deck.add_card(card_to_add)
+
+        size_after = deck.size
+        self.assertEqual(size_after - size_before, 1, "Card should be added only once")
+
+        self.assertNotEqual(
+            deck.draw(), card_to_add, "Cards should not be added to the top"
+        )
+
+    def test_draw(self):
+        deck = Deck(
+            [
+                Card("The Thing", "You are The Thing", CardKind.ROLE),
+                Card(
+                    "Infected",
+                    "If you got this card from another player, you are infected!",
+                    CardKind.ROLE,
+                ),
+                Card("Flamethrower", "Kill an adjacent player", CardKind.ACTION),
+            ]
+        )
+
+        size_before = deck.size
+
+        drawn_card = deck.draw()
+
+        self.assertEqual(
+            Card("The Thing", "You are The Thing", CardKind.ROLE),
+            drawn_card,
+            "Not drawing from the top of the deck",
+        )
+
+        self.assertEqual(deck.size - size_before, -1, "Deck did not shrink")
 
 
 if __name__ == "__main__":
