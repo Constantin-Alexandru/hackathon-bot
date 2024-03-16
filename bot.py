53e10f5e2b506740e95425e2da6a64577d170ee2
Alex Constantin <constantinalexandruaurel@yahoo.com>
2024-03-16 15:36:17 +0000

    Initial bot state

---
+++ bot.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+    
+@commands.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+def create_client() -> commands.Bot:
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = commands.Bot(command_prefix="!", intents=intents)
+    client.add_command(ping)
+
+    return client
---
+++ main.py
@@ -0,0 +1,9 @@
+from bot import create_client
+from config import config
+
+def main():
+    client = create_client()
+    client.run(config["BOT_KEY"])
+
+if __name__ == "__main__":
+    main()
